ac42bfb80c427b0fa3e97a6031ce6b6aaf1050b1
SamGarciaPereira <samgarciapereira@gmail.com>
2026-04-22 16:48:45 -0300

    dependencias atualizadas

--- public/js/app.js
+++ public/js/app.js
@@ -38877,8 +38877,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/nevana/Documentos/Energié/code-test/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/nevana/Documentos/Energié/code-test/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/samuel_gpr/Dev/code-test/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/samuel_gpr/Dev/code-test/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
